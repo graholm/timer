d6555a2f63fa048a7f9342b3f82f48a59876d760
graholm <graholm@nuevaschool.org>
2016-11-28 12:49:39 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
-
-function startTimer(duration, display) {
-  var start = Date.now(),
-      diff,
-      minutes,
-      seconds;
-  function timer() {
-    // get the number of seconds that have elapsed since 
-    // startTimer() was called
-    diff = duration - (((Date.now() - start) / 1000) | 0);
-
-    // does the same job as parseInt truncates the float
-    minutes = (diff / 60) | 0;
-    seconds = (diff % 60) | 0;
-
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    display.textContent = minutes + ":" + seconds; 
-
-    if (diff <= 0) {
-      // add one second so that the count down starts at the full duration
-      // example 05:00 not 04:59
-      start = Date.now() + 1000;
-    }
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
   };
-  // we don't want to wait a full second before the timer starts
-  timer();
-  setInterval(timer, 1000);
 }
 
-window.onload = function () {
-  var fiveMinutes = 60 * 5,
-      display = document.querySelector('#time');
-  startTimer(fiveMinutes, display);
-};
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv', deadline);
