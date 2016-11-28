256b0d4ba29c6f4b6b204baa2f28739f98a47f01
graholm <graholm@nuevaschool.org>
2016-11-28 12:46:31 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+
 function startTimer(duration, display) {
   var start = Date.now(),
       diff,
       minutes,
       seconds;
   function timer() {
-
+    // get the number of seconds that have elapsed since 
+    // startTimer() was called
     diff = duration - (((Date.now() - start) / 1000) | 0);
 
-
+    // does the same job as parseInt truncates the float
     minutes = (diff / 60) | 0;
     seconds = (diff % 60) | 0;
 
@@ -17,11 +19,12 @@ function startTimer(duration, display) {
     display.textContent = minutes + ":" + seconds; 
 
     if (diff <= 0) {
-  
+      // add one second so that the count down starts at the full duration
+      // example 05:00 not 04:59
       start = Date.now() + 1000;
     }
   };
-
+  // we don't want to wait a full second before the timer starts
   timer();
   setInterval(timer, 1000);
 }
